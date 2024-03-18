19f39336481ca3f3936657c223b245620f05f89b
wsrn829 <wsrn829@gmail.com>
2024-03-18 03:36:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ from peers.routers import peers
 from matching.routers import matching
 from tags.routers import tags
 import os
+import asyncpg
 
 app = FastAPI()
-app.include_router(authenticator.router)
-app.include_router(messages_router)
+
+@app.on_event("startup")
+async def startup():
+    app.state.pool = await asyncpg.create_pool(
+        user = os.getenv("DATABASE_USER")
+        password = os.getenv("DATABASE_PASSWORD")
+        database = os.getenv("DATABASE_NAME")
+        host = os.getenv("DATABASE_HOST")
+    )
+
+@app.on_event("shutdown")
+async def shutdown():
+    await app.state.pool.close()
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,16 +35,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(authenticator.router)
 app.include_router(accounts.router)
-app.include_router(peers.router)
+app.include_router(messages_router)
 app.include_router(matching.router)
 app.include_router(tags.router)
-
+app.include_router(peers.router)
 
 @app.get("/")
 def root():
     return {"message": "You hit the root path!"}
-
 
 @app.get("/api/launch-details")
 def launch_details():
