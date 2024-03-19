4e461ea0a0a069295ef7dd5d646d03cc281000b2
wsrn829 <wsrn829@gmail.com>
2024-03-18 21:32:04 -0400

    Update main.py, add .env

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,  HTTPException
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 # from authenticator import authenticator
 from messages.routers.messages import messages_router
